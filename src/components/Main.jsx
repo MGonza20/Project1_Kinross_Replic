a8a1b4f7a6b3287f2a484a1cc058e208288d7808
MGonza20 <pag20634@uvg.edu.gt>
2022-05-01 17:56:08 -0600

    arreglos en interfaz e implementando Intersection Observer

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import '../styles/main.scss';
 import LandingPage from './LandingPage';
 import WelcomeBanner from './WelcomeBanner';
@@ -18,12 +18,21 @@ import EmergentCookieScreen from './EmergentCookieScreen';
 function Main() {
   const [showMenu, setShowMenu] = useState(false);
   const [window, setWindow] = useState(false);
+  const wbRef = useRef();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      console.log(entry.isIntersecting);
+    });
+    observer.observe(wbRef.current);
+  }, []);
 
   return (
     <>
       <NavBar onAdd={() => setShowMenu(!showMenu)} show={showMenu} />
       <LandingPage />
-      <WelcomeBanner />
+      <WelcomeBanner propR={wbRef} />
       <ImgsText />
       <QuoteBanner />
       <OneImgInfo />
--- src/components/WelcomeBanner.jsx
+++ src/components/WelcomeBanner.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import '../styles/welcomeBanner.scss';
 import decor from '../media/svgs/decor.svg';
 
-function WelcomeBanner() {
+// eslint-disable-next-line react/prop-types
+function WelcomeBanner({ propR }) {
   return (
     <div className="textDiv">
       <div className="innerTextDiv">
-        <h1 className="welcomeText">
+        <h1 ref={propR} className="welcomeText">
           Welcome to Kinross House, the architectural jewel
           {' '}
           <br />
           {' '}
           in Scotland&apos;s crown.
         </h1>
-        <img className="miniDecoration" alt="miniDecoration" src={decor} />
+        <img className="miniDecorationnn" alt="miniDecoration" src={decor} />
       </div>
     </div>
   );
